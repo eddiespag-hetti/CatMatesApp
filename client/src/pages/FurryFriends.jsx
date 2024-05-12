6e0366698fa74402f0a004048f4016c5a9a28517
eddiespag-hetti <eddie_p89@hotmail.com>
2024-05-12 11:45:37 +1000

    altered cat cards in gallery to only contain a name and pic

--- client/src/pages/FurryFriends.jsx
+++ client/src/pages/FurryFriends.jsx
@@ -4,66 +4,18 @@ import "../index.css";
 const FurryFriends = () => {
   return (
     <div className="container">
-      <Card
-        imgSrc="./img/louie.png"
-        title="Louie"
-        description="Description of image 1."
-      />
-      <Card
-        imgSrc="./img/ovie.png"
-        title="Ovie"
-        description="Description of image 2."
-      />
-      <Card
-        imgSrc="./img/moira.png"
-        title="Moira"
-        description="Description of image 3."
-      />
-        <Card
-          imgSrc="./img/lola.png"
-          title="Lola"
-          description="Description of image 4."
-        />
-      <Card
-        imgSrc="./img/bobby.png"
-        title="Bobby"
-        description="Description of image 4."
-      />
-      <Card
-        imgSrc="./img/charlie.png"
-        title="Charlie"
-        description="Description of image 6."
-      />
-      <Card
-        imgSrc="./img/garfield.png"
-        title="Garfield"
-        description="Description of image 7."
-      />
-      <Card
-        imgSrc="./img/lily.png"
-        title="Lily"
-        description="Description of image 8."
-      />
-      <Card
-        imgSrc="./img/killer.png"
-        title="Killer"
-        description="Description of image 9."
-      />
-        <Card
-       imgSrc="./img/bronson.png"
-       title="Bronson"
-       description="Description of image 10."
-     />
-      <Card
-        imgSrc="./img/mavis.png"
-        title="Mavis"
-        description="Description of image 11."
-      />
-      <Card
-        imgSrc="./img/winston.png"
-        title="Winston"
-        description="Description of image 12."
-      />
+      <Card imgSrc="./img/louie.png" title="Louie" />
+      <Card imgSrc="./img/ovie.png" title="Ovie" />
+      <Card imgSrc="./img/moira.png" title="Moira" />
+      <Card imgSrc="./img/lola.png" title="Lola" />
+      <Card imgSrc="./img/bobby.png" title="Bobby" />
+      <Card imgSrc="./img/charlie.png" title="Charlie" />
+      <Card imgSrc="./img/garfield.png" title="Garfield" />
+      <Card imgSrc="./img/lily.png" title="Lily" />
+      <Card imgSrc="./img/killer.png" title="Killer" />
+      <Card imgSrc="./img/bronson.png" title="Bronson" />
+      <Card imgSrc="./img/mavis.png" title="Mavis" />
+      <Card imgSrc="./img/winston.png" title="Winston" />
     </div>
   );
 };
@@ -77,7 +29,7 @@ const Card = ({ imgSrc, title, description }) => {
       <img className="cat-portrait" src={imgSrc} alt={`Image of ${title}`} />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
-        <p className="card-description">{description}</p>
+
         <button className="card-button">Sit Me</button>
       </div>
     </div>
